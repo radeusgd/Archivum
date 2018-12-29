ac809ede85a9b7fb77d82708774ce36238c27d7b
Radosław Waśko <programistagd@gmail.com>
2018-12-29 16:06:38 +0100

    Added path string interpolator for easier getters in queries.

--- src/main/scala/com/radeusgd/archivum/querying/package.scala
+++ src/main/scala/com/radeusgd/archivum/querying/package.scala
@@ -1,8 +1,10 @@
 package com.radeusgd.archivum
 
-import com.radeusgd.archivum.datamodel.{DMNull, DMStruct, DMValue}
+import com.radeusgd.archivum.datamodel.{DMNull, DMStruct, DMUtils, DMValue}
 
 package object querying {
+
+   @deprecated
    def extractKey(key: String, safe: Boolean = true): (DMValue => DMValue) = {
       (v: DMValue) => {
          try {
@@ -12,5 +14,11 @@ package object querying {
             case e: ClassCastException => if (safe) DMNull else throw e
          }
       }
+   }
+
+   implicit class GetterHelper(val sc: StringContext) extends AnyVal {
+      def path(args: Any*): DMValue => DMValue =
+         DMUtils.makeGetter(sc.s(args))
+
    }
 }
